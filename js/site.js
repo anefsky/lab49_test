5e230420da3d14067e741281b70ade029223f468
Andrew Nefsky <anefsky@gmail.com>
2015-12-16 21:54:57 -0500

    first commit

---
+++ js/site.js
@@ -0,0 +1,41 @@
+var getToday = function() {
+		var date = new Date();
+		return date.getMonth() + '/' + date.getDate();
+};
+
+var makeDisplayVisible = function() {	
+	document.getElementById('display').style.visibility = 'visible';
+};
+
+var addBackgroundImage = function(image) {
+	document.getElementById('display').style.backgroundImage = 'url(' + image + ')';
+};
+
+
+var myApp = angular.module("myApp", []);
+
+myApp.controller('myCntl', function($scope, $http) {
+	$http.get("data/weather.json")
+	.then(function(response) {
+		$scope.cities = response.data.weather;
+		var cities = [];
+		for(var city in $scope.cities) {
+			cities.push(city);
+		}
+		$scope.cityList = cities;
+		$scope.today = getToday();
+		$('#city_selector').change(function() {
+			var citySelected = $(this).val();
+			var backgroundImage = $scope.cities[citySelected].image;
+
+			console.log('backgroundImage = ' + backgroundImage);
+
+			addBackgroundImage(backgroundImage);
+			$scope.citySelected = citySelected;
+			$scope.values = $scope.cities[citySelected];
+			$scope.$apply();
+			makeDisplayVisible();
+		});
+	});
+});
+
